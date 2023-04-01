57814d4bdd7301ea16f9547e1b8c7c00448678e7
engvictorbr <engenharia.victor@hotmail.com>
2023-04-01 02:31:44 -0300

    Main desafio feito, com musica e score

--- script.js
+++ script.js
@@ -2,11 +2,49 @@ const mario = document.querySelector('.mario');
 const cano = document.querySelector('.cano');
 const chao = document.querySelector('.chao');
 
+textStart = document.querySelector('iniciar')
+audioStart = new Audio('./audio/theme.mp3')
+somdepula = new Audio('./audio/pula.mp3')
+audioGameOver = new Audio('./audio/gameover.mp3')
+pontuacao = document.querySelector('pontos')
+
+var gol = -100
+
+
+//tentativa start
+const start = () => {
+
+    document.getElementById("iniciar").style.opacity = "0%";
+
+    cano.classList.add('canovindo');
+    chao.classList.add('chaopassando');
+    mario.src = './images/mario.gif';
+    mario.style.width = '100px';
+
+    audioStart.play();
+
+
+}
+document.addEventListener('keydown', start);
+
+//fim tentativa start
+
+
 const pula = () => {
 mario.classList.add('pula');
 setTimeout(() => {
     mario.classList.remove('pula');
 }, 600 );
+
+
+
+setTimeout(() => {
+ document.getElementById("score").innerHTML=`Score: ${gol=gol+100} pontos`;   
+}, 1000 )
+
+somdepula.play();
+
+
 }
 
 const loop = setInterval(() => {
@@ -22,8 +60,32 @@ const loop = setInterval(() => {
         mario.style.bottom = `${lugarmario}px`;
         mario.src = './images/perdeu.png';
         mario.style.width = "60px";
+        //tentativa game over
+        document.removeEventListener('keydown', pula);
+             document.getElementById("iniciar").style.color = "black";
+        document.getElementById("iniciar").style.opacity = "100%";
+            somdepula = null;
+        document.getElementById("iniciar").innerHTML="<strong>GAME OVER</strong> <br> Pressione F5 para reiniciar";
+
+
+            function stopAudioStart(){
+                audioStart.pause();
+                }stopAudioStart();
+                audioStart=null;
+
+            audioGameOver.play();
+
+            function stopAudio(){
+                audioGameOver.pause();
+                }setTimeout(stopAudio, 8000);
+
+            clearInterval(checkGameOver);
+            audioGameOver = null;
+
+            
+            //tentativa game over
     }
-},10)
+},10);
 
 
 document.addEventListener('keydown', pula);
